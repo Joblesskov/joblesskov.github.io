97167e7771c5b3268a7d425589ca603317fe729f
Joblesskov <104011538+Joblesskov@users.noreply.github.com>
2024-05-17 22:15:14 +0800

    Delete hw1 directory

--- hw1/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-let port = 8000;
-let app = express();
-    app.use(express.static('./static'));
-    app.listen(port, () => {
-    console.log(`Server started on port ${port}.\nPlease navigate to http://localhost:${port} in your browser.`)
-});
